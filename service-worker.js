eb29a36b0a42ae52e4f187e6f5f1921b6e4b1e33
AndrewTour <andrew.tour8@gmail.com>
2026-07-30 12:14:39 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='agnt-v119-8-hot-spotting-flow';
+const CACHE='agnt-v119-9-hot-spotting-cards';
 const ASSETS=['./','./index.html','./styles.css?v=119.8-hot-spotting-flow','./app.js?v=119.8-hot-spotting-flow','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
