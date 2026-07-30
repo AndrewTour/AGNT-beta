0fa934b91b92c083603e445c50025539f684dc50
AndrewTour <andrew.tour8@gmail.com>
2026-07-30 10:38:48 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='agnt-v119-5-market-pulse-prototype';
-const ASSETS=['./','./index.html','./styles.css?v=119.5-market-pulse-prototype','./app.js?v=119.5-market-pulse-prototype','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='agnt-v119-6-market-pulse-sessions';
+const ASSETS=['./','./index.html','./styles.css?v=119.6-market-pulse-sessions','./app.js?v=119.6-market-pulse-sessions','./firebase-config.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
